c70e638508955ea9e9d315674328a0f3b36ee230
xmdhs <xmdhss@gmail.com>
2022-07-13 12:04:06 +0800

    uuid

--- chunk/chunk.go
+++ chunk/chunk.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"compress/gzip"
 	"compress/zlib"
+	"encoding/binary"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
@@ -14,15 +16,16 @@ import (
 
 	"github.com/Tnze/go-mc/nbt"
 	"github.com/Tnze/go-mc/save/region"
+	"github.com/xmdhs/maptranslate/model"
 )
 
-func PaseMca[K any](f io.ReadWriteSeeker, filePath string) (Region[map[string]string], error) {
+func PaseMca[K any](f io.ReadWriteSeeker, filePath string) (Region[map[string]Entities], error) {
 	rg, err := region.Load(f)
 	if err != nil {
-		return Region[map[string]string]{}, fmt.Errorf("PaseMca: %w", err)
+		return Region[map[string]Entities]{}, fmt.Errorf("PaseMca: %w", err)
 	}
 	defer rg.Close()
-	cl := make([]Chunk[map[string]string], 0)
+	cl := make([]Chunk[map[string]Entities], 0)
 	for x := 0; x < 32; x++ {
 		for y := 0; y < 32; y++ {
 			if !rg.ExistSector(x, y) {
@@ -30,25 +33,30 @@ func PaseMca[K any](f io.ReadWriteSeeker, filePath string) (Region[map[string]st
 			}
 			b, err := rg.ReadSector(x, y)
 			if err != nil {
-				return Region[map[string]string]{}, fmt.Errorf("PaseMca: %w", err)
+				return Region[map[string]Entities]{}, fmt.Errorf("PaseMca: %w", err)
 			}
 			b, err = mcDecompress(b)
 			if err != nil {
-				return Region[map[string]string]{}, fmt.Errorf("PaseMca: %w", err)
+				return Region[map[string]Entities]{}, fmt.Errorf("PaseMca: %w", err)
 			}
 			var v K
 			err = nbt.Unmarshal(b, &v)
 			if err != nil {
-				return Region[map[string]string]{}, fmt.Errorf("PaseMca: %w", err)
+				return Region[map[string]Entities]{}, fmt.Errorf("PaseMca: %w", err)
 			}
-			cl = append(cl, Chunk[map[string]string]{
+			var pos model.NbtPosUUID
+			err = nbt.Unmarshal(b, &v)
+			if err != nil {
+				return Region[map[string]Entities]{}, fmt.Errorf("PaseMca: %w", err)
+			}
+			cl = append(cl, Chunk[map[string]Entities]{
 				X:    x,
 				Z:    y,
-				Data: getStrPath(v, ""),
+				Data: getStrPath(v, "", pos),
 			})
 		}
 	}
-	return Region[map[string]string]{
+	return Region[map[string]Entities]{
 		FilePath: filePath,
 		Chunk:    cl,
 	}, nil
@@ -129,14 +137,14 @@ type Chunk[K any] struct {
 	Data K
 }
 
-func getStrPath(v any, path string) map[string]string {
+func getStrPath(v any, path string, pos model.NbtPosUUID) map[string]Entities {
 	dt := reflect.TypeOf(v)
 	dv := reflect.ValueOf(v)
 	if dv.Kind() != reflect.Struct {
 		panic("nor struct")
 	}
 	l := reflect.VisibleFields(dt)
-	sm := make(map[string]string)
+	sm := make(map[string]Entities)
 
 	for _, t := range l {
 		v := dv.FieldByIndex(t.Index)
@@ -157,7 +165,7 @@ func getStrPath(v any, path string) map[string]string {
 			nPath = path + "." + name
 		}
 		if v.Kind() == reflect.Struct {
-			m := getStrPath(v.Interface(), name)
+			m := getStrPath(v.Interface(), name, pos)
 			for k, v := range m {
 				sm[k] = v
 			}
@@ -170,46 +178,99 @@ func getStrPath(v any, path string) map[string]string {
 		v = v.Elem()
 		m, ok := mapify(v.Interface())
 		if ok {
-			getStrPathMap(m, nPath, &sm)
+			getStrPathMap(m, nPath, &sm, Entities{}, pos)
 		}
 		if v.Kind() == reflect.Slice {
-			getStrPathSlice(v.Interface(), nPath, &sm)
+			getStrPathSlice(v.Interface(), nPath, &sm, Entities{}, pos)
 		}
 	}
 	return sm
 }
 
-func getStrPathMap(m map[string]any, path string, sm *map[string]string) {
+func getStrPathMap(m map[string]any, path string, sm *map[string]Entities, e Entities, pos model.NbtPosUUID) {
 	for k, v := range m {
 		rt := reflect.TypeOf(v)
 		rk := rt.Kind()
 		switch rk {
 		case reflect.Slice:
-			getStrPathSlice(v, path+"."+k, sm)
+			getStrPathSlice(v, path+"."+k, sm, e, pos)
 		case reflect.Map:
-			getStrPathMap(v.(map[string]any), path+"."+k, sm)
+			getStrPathMap(v.(map[string]any), path+"."+k, sm, e, pos)
 		case reflect.String:
-			(*sm)[path+"."+k] = v.(string)
+			e.TEXT = v.(string)
+			(*sm)[path+"."+k] = e
 		}
 	}
 }
 
-func getStrPathSlice(l any, path string, sm *map[string]string) {
+func getStrPathSlice(l any, path string, sm *map[string]Entities, e Entities, pos model.NbtPosUUID) {
 	rl := reflect.ValueOf(l)
 	rlen := rl.Len()
+	needList := false
+	if strings.HasSuffix("Entities", path) || strings.HasSuffix("TileEntities", path) || strings.HasSuffix("block_entities", path) {
+		needList = true
+	}
 
 	for i := 0; i < rlen; i++ {
 		ri := rl.Index(i)
 		if ri.Kind() == reflect.Interface {
 			ri = ri.Elem()
 		}
+		if needList {
+			switch path {
+			case "Level.Entities":
+				e = posUUID2Ent(pos.Level.Entities[i])
+			case "Level.TileEntities":
+				e = posUUID2Ent(pos.Level.TileEntities[i])
+			case "block_entities":
+				e = posUUID2Ent(pos.BlockEntities[i])
+			case "Entities":
+				e = posUUID2Ent(pos.Entities[i])
+			}
+		}
 		switch ri.Kind() {
 		case reflect.Slice:
-			getStrPathSlice(ri.Interface(), path+"["+strconv.Itoa(i)+"]", sm)
+			getStrPathSlice(ri.Interface(), path+"["+strconv.Itoa(i)+"]", sm, e, pos)
 		case reflect.Map:
-			getStrPathMap(ri.Interface().(map[string]any), path+"["+strconv.Itoa(i)+"]", sm)
+			getStrPathMap(ri.Interface().(map[string]any), path+"["+strconv.Itoa(i)+"]", sm, e, pos)
 		case reflect.String:
-			(*sm)[path+"["+strconv.Itoa(i)+"]"] = ri.Interface().(string)
+			e.TEXT = ri.Interface().(string)
+			(*sm)[path+"["+strconv.Itoa(i)+"]"] = e
 		}
 	}
+}
+
+type Entities struct {
+	UUID string
+	POS  [3]int
+	TEXT string
+}
+
+func posUUID2Ent(pos model.PosUUID) Entities {
+	e := Entities{}
+	e.POS[0] = pos.X
+	e.POS[1] = pos.Y
+	e.POS[2] = pos.Z
+	if len(pos.UUID) != 0 {
+		bw := &bytes.Buffer{}
+		err := binary.Write(bw, binary.BigEndian, pos.UUID)
+		if err != nil {
+			panic(err)
+		}
+		e.UUID = hex.EncodeToString(bw.Bytes())
+	} else {
+		bw := &bytes.Buffer{}
+		err := binary.Write(bw, binary.BigEndian, pos.UUIDMost)
+		if err != nil {
+			panic(err)
+		}
+		err = binary.Write(bw, binary.BigEndian, pos.UUIDLeast)
+		if err != nil {
+			panic(err)
+		}
+		e.UUID = hex.EncodeToString(bw.Bytes())
+
+	}
+
+	return e
 }
--- model/chunk.go
+++ model/chunk.go
@@ -10,3 +10,23 @@ type Level struct {
 	Entities     interface{} `nbt:"Entities" json:"Entities,omitempty"`
 	TileEntities interface{} `nbt:"TileEntities" json:"TileEntities,omitempty"`
 }
+
+type NbtPosUUID struct {
+	Level         LevelPosUUID `json:"Level,omitempty"`
+	BlockEntities []PosUUID    `nbt:"block_entities" json:"block_entities,omitempty"`
+	Entities      []PosUUID    `nbt:"Entities" json:"Entities,omitempty"`
+}
+
+type LevelPosUUID struct {
+	Entities     []PosUUID `nbt:"Entities" json:"Entities,omitempty"`
+	TileEntities []PosUUID `nbt:"TileEntities" json:"TileEntities,omitempty"`
+}
+
+type PosUUID struct {
+	X         int `nbt:"x" json:"x"`
+	Y         int `nbt:"y" json:"y"`
+	Z         int `nbt:"z" json:"z"`
+	UUID      []int32
+	UUIDLeast int64
+	UUIDMost  int64
+}
